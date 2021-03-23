7e1b01bed716a92013eb53b800b2970a9c60b532
Etienne Cellier-Clarke <etiennecclarke@icloud.com>
2021-03-23 17:09:22 +0000

    update route file

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'landing';
 $route['admin'] = 'Admin_Controller/Add_Movie_Controller';
 $route['add_genre'] = 'Admin_Controller/Add_Movie_Controller/add_genre';
 $route['add_actor'] = 'Admin_Controller/Add_Movie_Controller/add_actor';
