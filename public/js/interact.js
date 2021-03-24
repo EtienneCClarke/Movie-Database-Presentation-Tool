53f398a0199b797a30a9593203d453dcb9bb11e8
Etienne Cellier-Clarke <etiennecclarke@icloud.com>
2021-03-24 14:40:14 +0000

    added float animation to explore button

--- public/js/interact.js
+++ public/js/interact.js
@@ -11,5 +11,11 @@ var hero_filler_svg = document.getElementById('hero-filler-svgs');
 
 // When explore is clicked scroll popular section into view
 explore.onclick = function() {
+
+    // Scroll to explore
     hero_filler_svg.scrollIntoView({behavior: 'smooth'});
+
+    // Stop explore button floating
+    explore.style.animationIterationCount = "1";
+
 }
