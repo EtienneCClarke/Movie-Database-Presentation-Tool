df1ea00ba4719105ce11a5523753a4c0b715c76f
hans2369 <l3g3nd80y@gmail.com>
2021-03-24 12:53:22 +0000

    added the url to 'search movies' button

--- application/views/landing_view.php
+++ application/views/landing_view.php
@@ -12,7 +12,7 @@
         <!------- Call To Action ------->
         <section id="hero">
             <div id="hero-content-container">
-                <a href="#" id="call-to-action">SEARCH MOVIES</a>
+                <a href="<?php echo base_url('./index.php/Browse');?>" id="call-to-action">SEARCH MOVIES</a>
                 <p>The number 1 <br/>desitnation <br/>for all your movie needs!</p>
             </div>
             <div id="hero-filler-svgs">
