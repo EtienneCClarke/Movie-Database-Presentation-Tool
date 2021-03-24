4e6bcac3f468cdff882e81da8ad233cc0c5fd379
Etienne Cellier-Clarke <etiennecclarke@icloud.com>
2021-03-24 15:49:11 +0000

    Added hover animation to 'search movies'

--- public/js/interact.js
+++ public/js/interact.js
@@ -8,13 +8,12 @@
 var explore = document.getElementById('explore');
 var exploreSVG = explore.getElementsByTagName('svg');
 var popular = document.getElementById('popular');
-var hero_filler_svg = document.getElementById('hero-filler-svgs');
 
 // When explore is clicked scroll popular section into view
 explore.onclick = function() {
 
     // Scroll to explore
-    hero_filler_svg.scrollIntoView({behavior: 'smooth'});
+    popular.scrollIntoView({behavior: 'smooth'});
 
     // Stop explore button floating
     exploreSVG[0].style.animationIterationCount = "1";
