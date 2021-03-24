e6b90dd453a37f102ce1e5fe3376dbcfb37a5aa3
hans2369 <l3g3nd80y@gmail.com>
2021-03-24 12:46:35 +0000

    added the browse view file. Linked both webpages for home and browse

---
+++ application/controllers/Browse.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Browse extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/Browse
+	 */
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->load->view('browse_view');
+	}
+}
+
+
+
+?>
