8c4b38922441d578f70c99a6d0830b66e5854eed
Etienne Cellier-Clarke <etiennecclarke@icloud.com>
2021-03-24 01:23:51 +0000

    added call to action on landing page

--- application/views/landing_view.php
+++ application/views/landing_view.php
@@ -1,10 +1,37 @@
 <html>
 
-    <?php include('head.php'); ?>
+    <head>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/navigation.css"/>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>public/css/landing.css"/>
+    </head>
 
     <body>
         <!------- Navigation ------->
         <?php include('navigation.php'); ?>
+
+        <!------- Call To Action ------->
+        <section id="hero">
+            <div id="hero-content-container">
+                <a href="#" id="call-to-action">SEARCH MOVIES</a>
+                <p>The number 1 <br/>desitnation <br/>for all your movie needs!</p>
+            </div>
+            <div id="hero-filler-svgs">
+
+                <svg width="362" height="155" viewBox="0 0 362 155" fill="none" xmlns="http://www.w3.org/2000/svg" class="hero-svg">
+                    <path d="M201.974 1.77652C276.471 -11.4092 297.326 52.0371 298.441 85.4085C375.615 64.4089 366.656 123.053 352.53 155H17.4026C1.78937 127.733 -17.5042 67.0949 30.2278 42.6768C77.9598 18.2587 115.915 60.9904 128.926 85.4085C122.234 63.0252 127.476 14.9623 201.974 1.77652Z" fill="white"/>
+                </svg>
+
+
+                <svg width="175" height="64" viewBox="0 0 175 64" fill="none" xmlns="http://www.w3.org/2000/svg" class="hero-svg">
+                    <path d="M97.6392 0.733532C133.653 -4.71091 143.735 21.4863 144.274 35.2654C181.582 26.5947 177.251 50.809 170.422 64H8.41289C0.865051 52.7414 -8.46193 27.7037 14.6129 17.6214C37.6877 7.53908 56.0362 25.1831 62.3261 35.2654C59.0913 26.0233 61.6252 6.17798 97.6392 0.733532Z" fill="white"/>
+                </svg>
+
+                <svg width="373" height="146" viewBox="0 0 373 146" fill="none" xmlns="http://www.w3.org/2000/svg" class="hero-svg">
+                    <path d="M164.889 1.67337C88.1278 -10.7468 66.6392 49.0156 65.4901 80.4493C-14.0291 60.669 -4.79779 115.908 9.75774 146H355.069C371.156 120.316 391.036 63.1991 341.854 40.1988C292.671 17.1985 253.563 57.449 240.156 80.4493C247.051 59.3657 241.65 14.0935 164.889 1.67337Z" fill="white"/>
+                </svg>
+
+            </div>
+        </section>
     </body>
 
 </html>
