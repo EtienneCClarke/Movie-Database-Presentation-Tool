2738f64217ff370fe5ee270e2d52e0b9b000ec45
hans2369 <l3g3nd80y@gmail.com>
2021-04-05 12:46:41 +0100

    prepared sql statments in the search model

---
+++ application/models/movie_preview_model.php
@@ -0,0 +1,8 @@
+<?php
+class movie_preview_model extends CI_Model{
+
+    public function __construct()
+    {
+        $this->load->database(); 
+    }
+}
--- application/models/search_model.php
+++ application/models/search_model.php
@@ -5,3 +5,33 @@ class search_model extends CI_Model{
     {
         $this->load->database(); 
     }
+
+    public function search($input)
+    {
+        // scan movie titles //
+        $title_query = $this->db->query("SELECT title FROM movie 
+                                         WHERE title LIKE '%$input%'"); 
+        (if $title_query->num_rows() >= 1)
+        {
+            return $title_query->result(); 
+        }else{
+            // scan actors //
+            $actor_query = $this->db->query("SELECT firstname, lastname FROM actor
+                                             WHERE firstname LIKE '%$input%'
+                                             OR lastname LIKE '%$input%'"); 
+            (if $actor_query->num_rows() >= 1)
+            {
+                return $actor_query->result(); 
+            }else{
+                // scan directors
+                $director_query = $this->db->query("SELECT firstname, lastname FROM director
+                                             WHERE firstname LIKE '%$input%'
+                                             OR lastname LIKE '%$input%'"); 
+                (if $director_query->num_rows() >= 1)
+                {
+                    return $director_query->result(); 
+                }
+            }
+        }
+    }
+}
