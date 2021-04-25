2edea169d3e96df88513ddeaae50c8e33b43745c
hans2369 <l3g3nd80y@gmail.com>
2021-04-25 19:03:00 +0100

    preview page working

--- application/views/preview_view.php
+++ application/views/preview_view.php
@@ -10,15 +10,22 @@
         <!------- Navigation ------->
         <?php include('navigation.php'); ?>
 
+            <?php 
+                if(is_array($movie_details) || is_object($movie_details))
+                    {
+                        foreach($movie_details as $row)
+                        { 
+            ?>
         <div id='movie-container'>
             <div class='movie-image-container'>
-            <img src="<?php echo base_url('public/images/rushHour.jpg') ?>" width='100%' height='100%'>
+            <img src="<?php echo base_url($row->movieImage); ?>" width='100%' height='100%'>
+                
                 <div class='duration-rating-content'>
                     <section id='duration'>
-                        <h3>120min</h3> 
+                        <h3><?php echo $row->duration; ?>min</h3> 
                     </section> 
                     <section id='rating'>
-                        <h3>9.9
+                        <h3><?php echo $row->rating; ?>
                             <svg width="30" height="30" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9 0L11.0206 5.87336L17.5595 5.87336L12.2694 9.50329L14.2901 15.3766L9 11.7467L3.70993 15.3766L5.73056 9.50329L0.440492 5.87336L6.97937 5.87336L9 0Z" fill="#E83338"/>
                             </svg>
@@ -29,52 +36,44 @@
 
             <div class='movie-content'>
                 <section id='title'>
-                    <h1> Rush Hour </h1>
+                    <h1><?php echo $row->title; ?></h1>
                 </section>
 
                 <section id='contents'>
-                    <h2>Genre: <label>Action, Adventure</label></h2>
-                    <h2>Release Year: <label>2018</label></h2>
-                    <h2>Budget: <label>$4383478373.00</label></h2>
-                    <h2>Revenue: <label>$4903874573434.00</label></h2>
+                    <h2>Genre: <label><?php echo $row->genre; ?></label></h2>
+                    <h2>Release Year: <label><?php echo $row->year; ?></label></h2>
+                    <h2>Budget: <label>$<?php echo $row->budget; ?></label></h2>
+                    <h2>Revenue: <label>$<?php echo $row->revenue; ?></label></h2>
 
                     <h2>Directors:</h2>
-                    <?php
-                        for($i = 0; $i < 2; $i++)
-                            {
-                    ?>
-                        <div class='image-profile' style="display:inline-block">
-                            <img src="<?php echo base_url('public/images/Directors/brettRatner.jpg') ?>" title="Brett Ratner">
+                        <div class='image-profile'>
+                            <img src="<?php echo base_url($row->directorImage); ?>" title="<?php echo $row->director; ?>">
+                            <img src="<?php echo base_url($row->directorImage2); ?>" title="<?php echo $row->director2; ?>">
                         </div>
-                    <?php
-                            }
-                    ?>
 
                     <h2>Actors:</h2>
-                    <?php
-                        for($i = 0; $i < 3; $i++)
-                        {
-                    ?>
-                        <div class='image-profile' style="display:inline-block">
-                        <img src="<?php echo base_url('public/images/Actors/jackieChan.jpg') ?>" title="Jackie Chan">
+                        <div class='image-profile'>
+                        <img src="<?php echo base_url($row->actorImage) ?>" title="<?php echo $row->actor; ?>">
+                        <img src="<?php echo base_url($row->actorImage2) ?>" title="<?php echo $row->actor2; ?>">
+                        <img src="<?php echo base_url($row->actorImage3) ?>" title="<?php echo $row->actor3; ?>">
                         </div>
-                    <?php
-                        }
-                    ?>
                 </section> 
             </div>
         </div>
             <div id='story-container'>
                 <h1> Storyline </h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut iaculis luctus neque, lacinia semper magna gravida vel. Integer vulputate urna tellus, eget blandit tortor lobortis sed. Vestibulum interdum dui at vulputate bibendum. Nulla tristique at ligula eget feugiat. Integer volutpat velit ex. Aliquam semper tristique neque, ut finibus justo consequat eu. Sed neque augue, dignissim in rutrum sit amet, efficitur sed mi. Aliquam pellentesque mi ac purus imperdiet aliquam.
+                    <?php echo $row->description; ?>
                 </p>
 
                 <p>
-                    For more information <a href="">click here</a>.
+                    For more information <a href="<?php echo $row->hyperlink; ?>">click here</a>.
                 </p>
             </div>
-
+                <?php
+                    }
+                }
+            ?>
         <!------- Navigation ------->
         <?php include('footer.php'); ?>
 
